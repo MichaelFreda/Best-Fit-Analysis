1e93fda137e8d05f97f03e99072d2b1042b468fd
DerekChibbaro <>
2022-05-04 19:06:49 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+# Lab12
+# Michael & Derek
+
+
+"""
+1) Click on the Packages button on the left (the cube) and then search for and install Python's Natural Language Toolkit.
+    A] Type "nltk" into the search field.
+	B] Click on the + next to "nltk version 3.6.5.""
+	C] Watch in the Console while the package installs. When it's done, the green install message shows up in the Packages menu on the left and you're all set.
+
+2) Please create a fedPapLib.py file to hold your functions.
+	A] Start the code in that file with the lines below:
+
+import nltk
+nltk.download('punkt')
+nltk.download('stopwords')
+
+	B] Use the Shell to run your fedPapLib.py.
+	   The console should show a couple of packages downloading.
+
+	C] When that's done, comment out the 2nd and 3rd lines in your fedPapLib.py but leave "import nltk".
+
+3) Please create a textAns.txt file to hold your written answers.
+
+4) Please replace this comment with the names of your team then make your initial commit."""
