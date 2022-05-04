164165d3ced670ed402dfb08f31338c4d07f3f4e
DerekChibbaro <>
2022-05-04 19:35:27 +0000

    A2: Made function that calculates average word length of a list of strings

--- functions.py
+++ functions.py
@@ -8,4 +8,12 @@ def openPaper(num):
 def closePaper(num):
 	line = openPaper(num).read()
 	return line
-	
+
+def avgWLength(list):
+	tLetters = 0
+	wordCount = 0
+	for char in list:
+		tLetters += len(char)
+		wordCount += 1
+	average = tLetters/wordCount
+	return average
--- main.py
+++ main.py
@@ -3,25 +3,11 @@
 
 from functions import *
 
-print(closePaper(1))
-"""
-1) Click on the Packages button on the left (the cube) and then search for and install Python's Natural Language Toolkit.
-    A] Type "nltk" into the search field.
-	B] Click on the + next to "nltk version 3.6.5.""
-	C] Watch in the Console while the package installs. When it's done, the green install message shows up in the Packages menu on the left and you're all set.
+print(avgWLength(["Hi", "my", "name", "is", "Derek"]))
 
-2) Please create a fedPapLib.py file to hold your functions.
-	A] Start the code in that file with the lines below:
-
-import nltk
-nltk.download('punkt')
-nltk.download('stopwords')
-
-	B] Use the Shell to run your fedPapLib.py.
-	   The console should show a couple of packages downloading.
-
-	C] When that's done, comment out the 2nd and 3rd lines in your fedPapLib.py but leave "import nltk".
-
-3) Please create a textAns.txt file to hold your written answers.
-
-4) Please replace this comment with the names of your team then make your initial commit."""
+assert avgWLength(["I","am","hungry"]) == 3, "Wrong average"
+assert avgWLength(["Hi", "there", "pal"]) == 10/3, "Wrong average"
+assert avgWLength(["Uh", "ohhh", "I", "am", "sad"]) == 12/5, "Wrong average"
+assert avgWLength(["Last", "lab", "yes", "sir!"]) == 14/4, "Wrong average"
+assert avgWLength(["This", "is", "a", "test"]) == 11/4, "Wrong average"
+print("Success!")
