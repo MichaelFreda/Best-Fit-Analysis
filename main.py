7f9aeeba5851f1d1c56e9fb439dd36ec447724fd
fredam2025 <>
2022-05-11 19:12:50 +0000

    B7 - Printed out first 7 sentences using sent_tokenize function

--- main.py
+++ main.py
@@ -7,19 +7,10 @@
 from functions import *
 
 text = nltk.sent_tokenize(getText(1))
-text2 = nltk.sent_tokenize(getText(2))
-text3 = nltk.sent_tokenize(getText(3))
-text4 = nltk.sent_tokenize(getText(4))
-text5 = nltk.sent_tokenize(getText(5))
-#print(text)
+# text2 = nltk.sent_tokenize(getText(2))
+# text3 = nltk.sent_tokenize(getText(3))
+# text4 = nltk.sent_tokenize(getText(4))
+# text5 = nltk.sent_tokenize(getText(5))
 
-#print(avgPunMarks(text))
-#print(avgPunMarks(text2))
-#print(avgPunMarks(text3))
-#print(avgPunMarks(text4))
-#print(avgPunMarks(text5))
-assert avgPunMarks(text2) == 10/7, "Wrong average"
-assert avgPunMarks(text3) == 4/3, "Wrong average"
-assert avgPunMarks(text4) == 1, "Wrong average"
-assert avgPunMarks(text5) == 7/5, "Wrong average"
-print("Success!")
+print(text[:7])
+
