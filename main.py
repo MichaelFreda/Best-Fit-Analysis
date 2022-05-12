a4179a71a231954a12743db279e813211de916e0
fredam2025 <>
2022-05-12 01:14:37 +0000

    C3 - Displayed average fingerprints for Father Coughlin and MLK

--- main.py
+++ main.py
@@ -6,5 +6,13 @@
 
 from functions import *
 
-numList = [1, 2, 3]
-print(multFP(numList))
+# list of Father Coughlin's speeches
+fatherList = [1, 2, 3]
+# list of MLK's speeches
+mlkList = [7, 8, 9]
+# average fingerprint of Father Coughlin's speeches
+print("Father Coughlin's average fingerprint:")
+print(multFP(fatherList))
+# average fingerprint of MLK's speeches
+print("Martin Luther King Jr's average fingerprint:")
+print(multFP(mlkList))
